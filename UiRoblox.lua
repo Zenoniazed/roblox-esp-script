d3c554d131f4f087d3b168467b1ed1f2715fa911
AhmadV <86684222+Zenoniazed@users.noreply.github.com>
2026-05-08 10:52:52 +0700

    Update UiRoblox.lua

--- UiRoblox.lua
+++ UiRoblox.lua
@@ -4740,7 +4740,7 @@ function Library:Window(p)
             CloseUIShadow.BackgroundColor3 = Color3.fromRGB(163,162,165)
             CloseUIShadow.BackgroundTransparency = 1
             CloseUIShadow.Position = UDim2.new(0, 20, 0.2, 0) -- Vị trí nút
-            CloseUIShadow.Size = UDim2.new(0, 50, 0, 50) -- Kích thước nút vuông
+            CloseUIShadow.Size = UDim2.new(0, 60, 0, 60) -- Kích thước nút vuông
             CloseUIShadow.Image = "rbxassetid://1316045217"
             CloseUIShadow.ImageColor3 = Color3.fromRGB(24,24,31)
             CloseUIShadow.ImageTransparency = 0.5
